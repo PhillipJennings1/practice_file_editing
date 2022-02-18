317fcb229e638844c714510b144ff973e804392f
Phillip Jennings <p1jennings31@gmail.com>
2022-02-17 22:02:58 -0500

    editing imports and added external package

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import csv
+
+
+from matplotlib import pyplot as plt
+from csv import DictWriter as dw
+
 
 with open('companies.csv', 'w') as csvfile:
     x = ['name', 'type', 'age']
-    writer = csv.DictWriter(csvfile, fieldnames=x)
+    writer = dw(csvfile, fieldnames=x)
     writer.writeheader()
     writer.writerow({'name': 'Codecademy', 'type': 'Learning', 'age': '6'})
     writer.writerow({'age': '3'})
